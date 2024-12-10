c244ecb1f7c44e1ba1dda3c8468128423261317c
Lisa Johnson <lisa.johnson.4@ndus.edu>
2024-12-10 14:18:52 -0600

    update 3.

--- Migrations/20241208163011_userandregisterupdate.Designer.cs
+++ Migrations/20241208163011_userandregisterupdate.Designer.cs
@@ -3,6 +3,7 @@ using DogAdoptionWebsite.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DogAdoptionWebsite.Migrations
 {
     [DbContext(typeof(DogAdoptionWebsiteContext))]
-    partial class DogAdoptionWebsiteContextModelSnapshot : ModelSnapshot
+    [Migration("20241208163011_userandregisterupdate")]
+    partial class userandregisterupdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
